60be1c1e36e1bd7a82b52704290835836a094607
Nicolas Tello <cesarntello@gmail.com>
2021-07-29 20:31:15 -0300

    NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,19 +16,20 @@ export default function NavBar(){
       <Image src="holder.js/100px250" fluid />
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">techMarket</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
+          <NavDropdown title="Categorias" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Componentes</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Perifericos</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Accesorios</NavDropdown.Item>
+              {/* <NavDropdown.Divider /> */}
+              <NavDropdown.Item href="#action/3.4">Software</NavDropdown.Item>
+            </NavDropdown>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
+            
           </Nav>
           <Nav>
             <Nav.Link href="#deets">More deets</Nav.Link>
